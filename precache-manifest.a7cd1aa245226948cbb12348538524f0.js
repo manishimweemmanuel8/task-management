5b5c905f68121256e2a202411cc1a9285f115d93
manishimweemmanuel8 <manishimweemmanuel8@users.noreply.github.com>
2021-07-22 14:31:34 +0000

    deploy: 05ac02041117c9994c0cc2de0d75e45f9a2a918f

---
+++ precache-manifest.a7cd1aa245226948cbb12348538524f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad2c5a824b496269c91d06433a83f1c",
+    "url": "https://manishimweemmanuel8/task-management/index.html"
+  },
+  {
+    "revision": "96d35f428c0d378e0c4d",
+    "url": "https://manishimweemmanuel8/task-management/static/css/main.34728c2f.chunk.css"
+  },
+  {
+    "revision": "35f6e6923ee4b73a3683",
+    "url": "https://manishimweemmanuel8/task-management/static/js/2.0e543f75.chunk.js"
+  },
+  {
+    "revision": "96d35f428c0d378e0c4d",
+    "url": "https://manishimweemmanuel8/task-management/static/js/main.2a84fb0b.chunk.js"
+  },
+  {
+    "revision": "dc12fb52216ab0ed9a47",
+    "url": "https://manishimweemmanuel8/task-management/static/js/runtime~main.20229ed0.js"
+  }
+]);
